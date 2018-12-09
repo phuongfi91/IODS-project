9eae2d2dc6baaf0fdeee1188ad427a8110cf61c5
Phuong Nguyen <phuong.fi.91@gmail.com>
2018-12-09 22:09:10 +0200

    Add initial chapter 6 assigment files

---
+++ data/meet_and_repeat.R
@@ -0,0 +1,13 @@
+# Name: Phuong Nguyen
+# Date: 09.12.2018
+# This file is where data wrangling is done for the last week exercise
+# Original data source: 
+# https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/BPRS.txt
+# https://raw.githubusercontent.com/KimmoVehkalahti/MABS/master/Examples/data/rats.txt
+  
+
+
+
+### 
+
+# 1. 
